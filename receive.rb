416002b2c595350e9ecd68dca35c6f7df8d1ce87
Todd Morningstar <todd@thoughtbot.com>
2019-09-25 16:21:38 -0700

    Create Message Consumer Script :postbox:

---
+++ receive.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require "bunny"
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue("hello")
+
+begin
+  puts " [*] Waiting for messages. To exit press CTRL+C"
+  queue.subscribe(block: true) do |_delivery_info, _properties, body|
+    puts " [x] Received #{body}"
+  end
+rescue Interrupt => _
+  connection.close
+  puts " [👋] You are no longer listening to me talk!"
+
+  exit(0)
+end
+
