74cbc6c1bb4d81ebae2dbbaaeee211015dc3ec5d
Todd Morningstar <todd@thoughtbot.com>
2019-09-25 16:52:24 -0700

    Implement Simulated Worker Queue :woman_factory_worker:

---
+++ two/new_task.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require "bunny"
+
+message = ARGV.empty? ? 'Hello World!' : ARGV.join(" ")
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+
+queue = channel.queue("hello")
+
+queue.publish(message, persistent: true)
+
+puts " [x] Sent #{message}"
+
+connection.close
+
---
+++ two/worker.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "bunny"
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue("hello")
+
+begin
+  puts " [*] Waiting for messages. To exit press CTRL+C"
+  queue.subscribe(block: true) do |delivery_info, _properties, body|
+    puts " [x] Received #{body}"
+    sleep(body.count(".").to_i)
+    puts " [x] Done"
+  end
+rescue Interrupt => _
+  connection.close
+  puts " [👋] You are, once again, no longer listening to me talk!"
+
+  exit(0)
+end
+
