562c94aac396742e6d7f88f4f597ddd1e199a036
Todd <stratigos@users.noreply.github.com>
2019-09-26 12:11:17 -0700

    Add Tutorial 3: First Fanout Scripts :dash:
    * Add `emit_log.rb` script to create a `fanout` exchange, which will
      send messages to all queues
    
    * Add Fanout Consumer
      * 📝 Note: when receiving a message with non ASCII characters, `Bunny`
        throws an exception for mixed encodings. It can still `puts` out
        some emoji just fine though 😉

---
+++ three/receive_logs.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "bunny"
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+exchange = channel.fanout("logs")
+queue = channel.queue("", exclusive: true)
+
+queue.bind(exchange)
+
+puts " [*] 🛏 Waiting for logs. 🚪 To exit press CTRL+C"
+
+begin
+  queue.subscribe(block: true) do |_delivery_info, _properties, body|
+    puts " [x] 📬 #{body}"
+  end
+rescue Interrupt => _
+  puts " [👋] CYUH! 💨 "
+  channel.close
+  connection.close
+end
