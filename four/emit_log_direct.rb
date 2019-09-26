c23578d117656b324124300e66266b4600cf627a
Todd Morningstar <techarchist@gmail.com>
2019-09-26 12:23:39 -0700

    Add Tutorial Four: Produce Log Levels :level_slider:
    * Add script for detecting log severity, and broadcasting to a key
      named for a severity.

---
+++ four/emit_log_direct.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require "bunny"
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+exchange = channel.direct("direct_logs")
+severity = ARGV.shift || "info"
+message = ARGV.empty? ? "Hello World!!!" : ARGV.join(" ")
+
+exchange.publish(message, routing_key: severity)
+
+puts " [x] 📭 Sent message: '#{message}'"
+
+connection.close
